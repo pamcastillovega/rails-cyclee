6eee08e09787cf5c4f612f66d2e6792fa130053c
Pamela Castillo Vega <pamela.castillo.vega@gmail.com>
2022-08-30 16:25:55 +0800

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "lanes#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lanes do
+    resources :reviews
+  end
+
+  resources :users do
+    resources :parking_histories
+  end
+
+  resources :parking_locations do
+    resources :reports
+  end
 end
