ff9f31d0522405722ea00b0da3384e386d678d40
Abdul Rafi <arafi15@gmail.com>
2022-08-30 16:59:05 +0800

    setup ongoing

--- app/controllers/lanes_controller.rb
+++ app/controllers/lanes_controller.rb
@@ -1,2 +1,12 @@
 class LanesController < ApplicationController
+  def index
+    @lanes = Lane.all
+    # @markers = @lanes.geocoded.map do |lane|
+    #   {
+    #     lat: lane.latitude,
+    #     lng: lane.longitude,
+    #     info_window: render_to_string(partial: "info_window", locals: { lane: lane })
+    #   }
+    # end
+  end
 end
--- app/controllers/parking_locations_controller.rb
+++ app/controllers/parking_locations_controller.rb
@@ -1,2 +1,12 @@
 class ParkingLocationsController < ApplicationController
+  def index
+    @parkinglocations = ParkingLocations.all
+    @markers = @parkinglocations.geocoded.map do |location|
+      {
+        lat: location.latitude,
+        lng: location.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { location: location })
+      }
+    end
+  end
 end
