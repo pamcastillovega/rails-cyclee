4a95d2d9ee19139b675376a3862eca5ec465d566
Abdul Rafi <arafi15@gmail.com>
2022-08-30 15:47:15 +0800

    geocode partial set up

--- app/models/parking_location.rb
+++ app/models/parking_location.rb
@@ -1,4 +1,6 @@
 class ParkingLocation < ApplicationRecord
   has_many :parking_histories
   has_many :reports
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_065947) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_073234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_065947) do
     t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "reports", force: :cascade do |t|
