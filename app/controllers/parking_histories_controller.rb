d3e1671cf9d0ee7a19333d41ec267fbfab321b44
sijing wong <swongsj@gmail.com>
2022-09-02 14:08:18 +0800

    saving..

--- app/controllers/parking_histories_controller.rb
+++ app/controllers/parking_histories_controller.rb
@@ -5,6 +5,7 @@ class ParkingHistoriesController < ApplicationController
   end
 
   def index
+
     @parking_history = ParkingHistory.find(params[:id])
     @parking_locations = ParkingLocation.for(@parking_history.user)
   end
