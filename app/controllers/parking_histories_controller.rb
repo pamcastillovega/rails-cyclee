e855d6178d0f00ea13973fc4793baf46a5a65e48
sijing wong <swongsj@gmail.com>
2022-09-02 15:38:46 +0800

    saving

--- app/controllers/parking_histories_controller.rb
+++ app/controllers/parking_histories_controller.rb
@@ -1,13 +1,11 @@
 class ParkingHistoriesController < ApplicationController
-  def show
+  # def show
     # @parking_history = ParkingHistory.find(params[:id])
-    # @parking_locations = ParkingLocation.for(@parking_history.user)
-  end
+  # end
 
   def index
-
-    @parking_history = ParkingHistory.find(params[:id])
-    @parking_locations = ParkingLocation.for(@parking_history.user)
+    @parking_histories = ParkingHistory.find(params[:user_id])
   end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,5 @@ class UsersController < ApplicationController
   end
 
 
+
 end
