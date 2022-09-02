375c19a20b143625c440ed7e9a657615cbdaf70a
sijing wong <swongsj@gmail.com>
2022-09-02 14:21:39 +0800

    saving

--- app/models/parking_history.rb
+++ app/models/parking_history.rb
@@ -2,5 +2,5 @@ class ParkingHistory < ApplicationRecord
   belongs_to :user
   belongs_to :parking_location
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # after_validation :geocode, if: :will_save_change_to_address?
 end
