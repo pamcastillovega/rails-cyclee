4fb8098033e075870974860000f05de95c71c3f8
sijing wong <swongsj@gmail.com>
2022-09-05 10:23:51 +0800

    map

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @parking_history = ParkingHistory.all
   end
+
+
+private
+
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :reports
   has_many :parking_histories
-  has_one_attached :photos
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
