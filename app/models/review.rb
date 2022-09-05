6299c380c81f742341097b8f1f96501bd5897cc5
sijing wong <swongsj@gmail.com>
2022-09-05 15:13:23 +0800

    cloud for lane review photo cloudinary

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :lane
   has_one_attached :photo
+  # has_many_attached :photos
   validates :comment, :rating, presence: true
   validates :rating, numericality: { only_integer: true }
 end
