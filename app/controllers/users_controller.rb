a0e186f3b720370e524b180f91fabc5f8152b443
Pamela Castillo Vega <pamela.castillo.vega@gmail.com>
2022-09-07 12:37:05 +0800

    created a mapbox snapshot for current parking location on user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @lng = @user.parking_histories.last.parking_location.longitude
+    @lat = @user.parking_histories.last.parking_location.latitude
   end
 end
