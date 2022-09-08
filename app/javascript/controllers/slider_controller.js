243d3d50c36216c5930d9711b4c9f4d84e6ba732
Pamela Castillo Vega <pamela.castillo.vega@gmail.com>
2022-09-08 09:54:22 +0800

    fixed errors

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="slider"
 export default class extends Controller {
-  static targets = ["slider-toggle", "wrap", "toggles"]
+  static targets = ["slider-toggle", "wrap"]
 
   connect() {
     console.log("connected slider controller")
@@ -13,10 +13,8 @@ export default class extends Controller {
 
     if(this.wrapTarget.classList.contains("active")) {
       this.wrapTarget.style.overflow = "scroll"
-      this.togglesTarget.style.display = "none"
     } else {
       this.wrapTarget.style.overflow = "visible"
-      this.togglesTarget.style.display = "block"
     }
   }
 
