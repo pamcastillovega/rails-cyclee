6282a123b1a7cec0aed9ada9d7bea1a991592950
Pamela Castillo Vega <pamela.castillo.vega@gmail.com>
2022-09-09 13:36:37 +0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,147 +50,147 @@ user_alex = User.create(
 )
 puts 'Created user'
 
-address1 = ParkingLocation.create(
+ParkingLocation.create(
   address: '288 Queen St Ws'
 )
 puts 'Parking Location created'
 
-address2 = ParkingLocation.create(
+ParkingLocation.create(
   address: '70 Lynn Williams St'
 )
 puts 'Parking Location created'
 
-address3 = ParkingLocation.create(
+ParkingLocation.create(
   address: '20 Sudbury St, Toronto'
 )
 puts 'Parking Location created'
 
-address4 = ParkingLocation.create(
+ParkingLocation.create(
   address: '100 Harbord St, Toronto'
 )
 puts 'Parking Location created'
 
-address5 = ParkingLocation.create(
+ParkingLocation.create(
   address: '180 Wellington St W'
 )
 puts 'Parking Location created'
 
-address6 = ParkingLocation.create(
+ParkingLocation.create(
   address: '1526 Queen St W'
 )
 puts 'Parking Location created'
 
-address7 = ParkingLocation.create(
+ParkingLocation.create(
   address: '106 Broadview Ave'
 )
 puts 'Parking Location created'
 
-ph1 = ParkingHistory.create(
-  parking_location: address1,
-  user: user_bob
-)
-puts 'Parking History created'
+# ph1 = ParkingHistory.create(
+#   parking_location: address1,
+#   user: user_bob
+# )
+# puts 'Parking History created'
 
-ParkingHistory.create(
-  parking_location: address2,
-  user: user_anna
-)
-puts 'Parking History created'
+# ParkingHistory.create(
+#   parking_location: address2,
+#   user: user_anna
+# )
+# puts 'Parking History created'
 
-ph3 = ParkingHistory.create(
-  parking_location: address3,
-  user: user_yc
-)
-puts 'Parking History created'
+# ph3 = ParkingHistory.create(
+#   parking_location: address3,
+#   user: user_yc
+# )
+# puts 'Parking History created'
 
-ParkingHistory.create(
-  parking_location: address4,
-  user: user_alex
-)
-puts 'Parking History created'
+# ParkingHistory.create(
+#   parking_location: address4,
+#   user: user_alex
+# )
+# puts 'Parking History created'
 
-Report.create(
-  parking_location: ph1.parking_location,
-  user: ph1.user,
-  date: '18/09/2022',
-  time: '13:00',
-  comment: 'Someone stole my pedals wtf'
-)
-puts 'Report created'
+# Report.create(
+#   parking_location: ph1.parking_location,
+#   user: ph1.user,
+#   date: '18/09/2022',
+#   time: '13:00',
+#   comment: 'Someone stole my pedals wtf'
+# )
+# puts 'Report created'
 
-Report.create(
-  parking_location: ph1.parking_location,
-  user: ph1.user,
-  date: '18/09/2022',
-  time: '13:00',
-  comment: 'Someone stole my pedals wtf'
-)
-puts 'Report created'
+# Report.create(
+#   parking_location: ph1.parking_location,
+#   user: ph1.user,
+#   date: '18/09/2022',
+#   time: '13:00',
+#   comment: 'Someone stole my pedals wtf'
+# )
+# puts 'Report created'
 
-Report.create(
-  parking_location: ph1.parking_location,
-  user: ph1.user,
-  date: '18/09/2022',
-  time: '13:00',
-  comment: 'Someone stole my pedals wtf'
-)
-puts 'Report created'
+# Report.create(
+#   parking_location: ph1.parking_location,
+#   user: ph1.user,
+#   date: '18/09/2022',
+#   time: '13:00',
+#   comment: 'Someone stole my pedals wtf'
+# )
+# puts 'Report created'
 
-Report.create(
-  parking_location: ph1.parking_location,
-  user: ph1.user,
-  date: '18/09/2022',
-  time: '13:00',
-  comment: 'Someone stole my pedals wtf'
-)
-puts 'Report created'
+# Report.create(
+#   parking_location: ph1.parking_location,
+#   user: ph1.user,
+#   date: '18/09/2022',
+#   time: '13:00',
+#   comment: 'Someone stole my pedals wtf'
+# )
+# puts 'Report created'
 
-Report.create(
-  parking_location: ph3.parking_location,
-  user: ph3.user,
-  date: '18/09/2022',
-  time: '15:00',
-  comment: 'Crazy man asking me to join his MLM business'
-)
-puts 'Report created'
+# Report.create(
+#   parking_location: ph3.parking_location,
+#   user: ph3.user,
+#   date: '18/09/2022',
+#   time: '15:00',
+#   comment: 'Crazy man asking me to join his MLM business'
+# )
+# puts 'Report created'
 
-Report.create(
-  parking_location: address2,
-  user: user_anna,
-  date: '18/09/2022',
-  time: '17:00',
-  comment: 'Saw someone touching bike chains and sniffing'
-)
-puts 'Report created'
+# Report.create(
+#   parking_location: address2,
+#   user: user_anna,
+#   date: '18/09/2022',
+#   time: '17:00',
+#   comment: 'Saw someone touching bike chains and sniffing'
+# )
+# puts 'Report created'
 
-Report.create(
-  parking_location: address4,
-  user: user_yc,
-  date: '18/09/2022',
-  time: '19:00',
-  comment: 'Saw Michael Myers lurking in that area yikes'
-)
-puts 'Report created'
+# Report.create(
+#   parking_location: address4,
+#   user: user_yc,
+#   date: '18/09/2022',
+#   time: '19:00',
+#   comment: 'Saw Michael Myers lurking in that area yikes'
+# )
+# puts 'Report created'
 
-USERS = [user_bob, user_anna, user_yc, user_alex]
+# USERS = [user_bob, user_anna, user_yc, user_alex]
 
 lanes["features"].each do |lane|
-  lane_obj = Lane.create(
+  Lane.create(
     coordinates: lane["geometry"]["coordinates"].first,
     name: lane["properties"]["STREET_NAME"],
     lane_type: lane["properties"]["INFRA_HIGHORDER"],
     objectid: lane["properties"]["OBJECTID"]
   )
   puts 'Lane created'
-
-    Review.create(
-    rating: rand(1..5),
-    lane: lane_obj,
-    user: USERS.sample,
-    comment: "What an interesting bike lane. Very interesting indeed."
-  ) if lane["properties"]["OBJECTID"].even?
-  puts 'Review created'
 end
+
+#     Review.create(
+#     rating: rand(1..5),
+#     lane: lane_obj,
+#     user: USERS.sample,
+#     comment: "What an interesting bike lane. Very interesting indeed."
+#   ) if lane["properties"]["OBJECTID"].even?
+#   puts 'Review created'
 
 all_lanes = Lane.all
 
