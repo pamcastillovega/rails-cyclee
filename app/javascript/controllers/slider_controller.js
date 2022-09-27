002d006a03b19d78ad2aee85475517c564f68f0d
Pamela Castillo Vega <pamela.castillo.vega@gmail.com>
2022-09-27 19:06:58 +0800

    updates to slider

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="slider"
 export default class extends Controller {
-  static targets = ["slider-toggle", "wrap"]
+  static targets = ["wrap", "slide"]
 
   connect() {
     console.log("connected slider controller")
@@ -11,13 +11,20 @@ export default class extends Controller {
   slideToggle() {
     this.wrapTarget.classList.toggle("active")
 
-    if(this.wrapTarget.classList.contains("active")) {
+    // this.wrapTarget.style.height = "0px"
 
-      this.wrapTarget.style.overflow = "hidden"
-      this.wrapTarget.style.overflow = "scroll"
-    } else {
-      this.wrapTarget.style.overflow = "visible"
-    }
+    // this.wrapTarget.classList.toggle("active")
+    // this.closeTarget.style.display = "block"
+
+    // if(this.wrapTarget.classList.contains("active")) {
+    //   this.slideTarget.style.display = "none"
+    // }
+
+    // if(this.wrapTarget.classList.contains("active")) {
+    //   this.wrapTarget.style.overflow = "hidden"
+    //   this.wrapTarget.style.overflow = "scroll"
+    // } else {
+    //   this.wrapTarget.style.overflow = "visible"
+    // }
   }
-
 }
